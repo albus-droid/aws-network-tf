781a9ff1583ce09bbceb7b796435d9b542d8baaa
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-03-07 14:32:09 -0500

    updated

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 # Add output variables
-output "subnet_id" {
-  value = aws_subnet.public_subnet[*].id
-}
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
+#output "subnet_id" {
+#  value = aws_subnet.public_subnet[*].id
+#}
+#
+#output "vpc_id" {
+#  value = aws_vpc.main.id
+#}
